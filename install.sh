ab4838515d5680e6fdd38c94e1a20a0198dc44d7
Alexander Svetlov <92632623+GigantPro@users.noreply.github.com>
2025-02-27 19:06:12 +0300

    Update script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install htop ranger zsh neofetch neovim ibus procps exa highlight zoxide git -y
 
 echo "Removing all files"
-find ../ -mindepth 1 -maxdepth 1 ! -name "server-auto-configuration" -exec rm -rf {} +
+find ../ -mindepth 1 -maxdepth 1 ! -name "server-auto-configuration" ! -name ".ssh" -exec rm -rf {} +
 
 echo "Copeing all config files"
 cp -r ./ ../
@@ -14,4 +14,8 @@ rm -rf ../README.md ../install.sh ../.git
 echo "Switch to zsh"
 chsh -s $(which zsh)
 
-reboot
+echo "Creating history file"
+mkdir ~/.cache
+mkdir ~/.cache/zsh
+touch ~/.cache/zsh/histfile
+#reboot
