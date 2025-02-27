a07cc2fa47a27f28998bc74d9103463fc1ff9bf1
Alexander Svetlov <92632623+GigantPro@users.noreply.github.com>
2025-02-27 19:14:00 +0300

    1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dependencies"
 sudo apt update && sudo apt upgrade -y
-sudo apt install htop ranger zsh neofetch neovim ibus procps exa highlight zoxide git -y
+sudo apt install htop ranger fzf zsh neofetch neovim ibus procps exa highlight zoxide git -y
 
 echo "Removing all files"
 find ../ -mindepth 1 -maxdepth 1 ! -name "server-auto-configuration" ! -name ".ssh" -exec rm -rf {} +
