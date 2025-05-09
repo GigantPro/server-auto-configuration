c20247f2dc514ef528f29fa9b132ab5ce12b67ef
Alexander Svetlov <92632623+GigantPro@users.noreply.github.com>
2025-05-09 19:50:53 +0300

    Update root_install.sh

--- root_install.sh
+++ root_install.sh
@@ -1,27 +1,57 @@
-#/bin/sh
+#!/bin/sh
 
-echo "Installing dependencies"
+echo "Обновляем систему и ставим зависимости"
 sudo apt update && sudo apt upgrade -y
-sudo apt install htop ranger fzf zsh neofetch neovim ibus procps exa highlight zoxide git -y
+sudo apt install \
+    htop \
+    ranger \
+    fzf \
+    zsh \
+    neofetch \
+    neovim \
+    ibus \
+    procps \
+    exa \
+    highlight \
+    zoxide \
+    git \
+    tmux \
+    wget \
+    nala \
+    python3 \
+    python3-pip \
+    python3-venv \
+    fd-find \
+  -y
 
-echo "Removing all files"
-find ../ -mindepth 1 -maxdepth 1 ! -name "server-auto-configuration" ! -name ".ssh" -exec rm -rf {} +
+# Чтобы команда `fd` работала (конфиг FZF), создаём симлинк:
+if [ -x "$(command -v fdfind)" ]; then
+  sudo ln -sf "$(command -v fdfind)" /usr/local/bin/fd
+fi
 
-echo "Copeing all config files"
-find ./ -mindepth 1 -maxdepth 1 ! -name ".ssh" -exec cp -r {} ../
-rm -rf ../README.md ../install.sh ../.git
+echo "Удаляем всё в домашней папке, кроме .ssh и папки с этим скриптом"
+find ../ -mindepth 1 -maxdepth 1 \
+     ! -name "server-auto-configuration" \
+     ! -name ".ssh" \
+  -exec rm -rf {} +
 
-echo "Switch to zsh"
-chsh -s $(which zsh)
+echo "Копируем все конфиги в домашнюю папку"
+find ./ -mindepth 1 -maxdepth 1 \
+     ! -name ".ssh" \
+  -exec cp -r {} ../ \;
+rm -rf ../README.md ../root_install.sh ../.git
 
-echo "Creating history file"
-mkdir ~/.cache
-mkdir ~/.cache/zsh
+echo "Меняем shell на zsh"
+chsh -s "$(which zsh)"
+
+echo "Создаём кеш для Z-sh"
+mkdir -p ~/.cache/zsh
 touch ~/.cache/zsh/histfile
 
-echo "Add ssh key"
-mkdir ~/.ssh
-touch ~/.ssh/authorized_keys
-echo ./.ssh/authorized_keys > ~/.ssh/authorized_keys
+echo "Устанавливаем ваш SSH-ключ"
+mkdir -p ~/.ssh
+cp ./.ssh/authorized_keys ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
 
-#reboot
+echo "Готово! Перезайдите по SSH — по паролю доступа уже нет, только по ключам."
